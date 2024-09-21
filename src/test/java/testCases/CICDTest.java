4e57012be0b328f80bcdee049d18b0e46f55faaa
Abthakur1 <absk.cts@gmail.com>
2024-09-21 20:35:08 +0530

    cicd test

---
+++ src/test/java/testCases/CICDTest.java
@@ -0,0 +1,10 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class CICDTest {
+    @Test
+    public void test1(){
+        System.out.println("cicd run with github actions");
+    }
+}
