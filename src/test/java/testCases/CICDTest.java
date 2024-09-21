1dff5717ca109d4266b500a2cde47df7b14c5f9b
Abthakur1 <absk.cts@gmail.com>
2024-09-21 21:15:20 +0530

    check with chrome

--- src/test/java/testCases/CICDTest.java
+++ src/test/java/testCases/CICDTest.java
@@ -2,13 +2,18 @@ package testCases;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class CICDTest {
     @Test
     public void test1(){
         System.out.println("cicd run with github actions");
-        WebDriver driver = new ChromeDriver();
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://www.google.co.in/");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.google.co.in/");
     }
 }
